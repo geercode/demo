38d1899b245191b0e755b22f5c0fc694d0f17abf
geercode <huifumanlove@hotmail.com>
2020-01-08 16:56:27 +0800

    改为gradle模式

---
+++ src/main/java/com/geercode/sample/demo/EleHallApp.java
@@ -0,0 +1,11 @@
+package com.geercode.sample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleHallApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EleHallApp.class, args);
+    }
+}
---
+++ src/main/java/com/geercode/sample/demo/web/TestController.java
@@ -0,0 +1,28 @@
+package com.geercode.sample.demo.web;
+
+import com.geercode.elehall.autoconfigure.web.common.SysResponseCode;
+import com.geercode.elehall.web.common.base.BaseCode;
+import com.geercode.elehall.web.common.base.BaseResp;
+import com.geercode.sample.demo.common.AppBaseCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @Autowired
+    private SysResponseCode sysResponseCode;
+
+    @GetMapping("/check")
+    public BaseResp check() {
+        String msg = BaseCode.getResponseCodeByCode(0).getMsg();
+        return BaseResp.custom(sysResponseCode.ARG_EMPTY, null);
+    }
+
+    @GetMapping("/appCheck")
+    public BaseResp appCheck() {
+        return BaseResp.custom(AppBaseCode.APP_ERROR, null);
+    }
+}
